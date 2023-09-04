a4778156406bec97cabb9f4b33612f44565981b1
chakrit <chakrit.s.w@gmail.com>
2023-09-04 11:31:56 +0700

    แก้ไข ชื่อตารางข้อหารือ และเพิ่ม model menubyrole

---
+++ src/app/models/menubyrole.ts
@@ -0,0 +1,11 @@
+export class Menubyrole {
+    menubyrole_id:number;
+    role_id:number;
+    display_name:string;
+    iconName_front:string;
+    iconName_back:string;
+    route:string;
+    level:number;
+    parent_id:number;
+    disable:number;
+}
